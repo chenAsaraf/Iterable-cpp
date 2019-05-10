fd2a496419b3b270739736d7fbb1485fb1caea0a
chenAsaraf <44566578+chenAsaraf@users.noreply.github.com>
2019-05-10 13:49:44 +0300

    Create range.hpp

---
+++ range.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+using namespace std;
+
+namespace itertools{
+	
+	template<typename T> class range{
+
+		public:
+		//Varaiables
+		T start;
+		T finish; 
+
+		//Constructor
+		range(T firstValue, T secondValue) : start(firstValue), finish(secondValue){}
+
+
+		//-------------------------------------------------------------------
+		// iterator related code:
+		// inner class and methods that return instances of it)
+		//-------------------------------------------------------------------
+		class iterator {
+			private:
+			T* value;
+			
+			public:
+			//Constructor
+			iterator(T* rangeStartElement):  value(rangeStartElement){}
+
+			// The method is const as this operator does not allow changing of the iterator.
+			// Note that it returns T& as it allows to change what it points to.
+			T operator*() const {
+				return *value;
+			}
+
+			T* operator->() const {
+				return &value;
+			}
+
+			// ++i; 
+			iterator* operator++() {
+				(*value)++;
+				return this;
+			}
+
+			// i++; 
+			// Usually iterators are passed by value and not by const& as they are small.
+			const iterator operator++(int) {
+				iterator tmp= *this;
+				(*value)++;
+				return tmp; 		
+			}
+
+			bool operator==(const iterator& rhs) const {
+				return *value != *(rhs.value);
+			}
+
+			bool operator!=(const iterator& rhs) const {
+				return *value != *(rhs.value);
+			}
+		}; //end iterator
+
+		iterator begin() {
+			return iterator{&start};
+		}
+		
+		iterator end() {
+			return iterator{&finish};
+		}
+
+
+	};//end of class range
+};//end namespace itertools
