1129703aa246ee28e3cc1f96ebf9e14f7f4663e9
danivngopro <44786079+danivngopro@users.noreply.github.com>
2019-05-13 15:08:34 +0300

    Add files via upload

--- zip.hpp
+++ zip.hpp
@@ -26,36 +26,19 @@ namespace itertools{
 			B value2;
 			public:
 			//Constructor
-			iterator(T firstStart, S secondStart):  value(firstStart),value2(secondStart){}
+			iterator(A firstStart, B secondStart):  value(firstStart),value2(secondStart){}
 
 			// The method is const as this operator does not allow changing of the iterator.
 			// Note that it returns T& as it allows to change what it points to.
 			T operator*() const {
 				return value;
 			}
-
-			T* operator->() const {
-				return &value;
-			}
-
 			// ++i; 
 			iterator* operator++() {
 				value++;
 				return this;
 			}
-
-			// i++; 
-			// Usually iterators are passed by value and not by const& as they are small.
-			const iterator operator++(int) {
-				iterator tmp= *this;
-				value++;
-				return tmp; 		
-			}
-
-			bool operator==(const iterator& other) const {
-				return value == (other.value);
-			}
-
+			
 			bool operator!=(const iterator& other) const {
 				return value != (other.value);
 			}
